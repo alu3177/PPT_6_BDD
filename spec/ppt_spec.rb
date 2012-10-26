f6945e3d501ec8fe33e568c1e242d77f4fc19efb
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-10-26 12:25:01 +0100

    Implementado primer test y el codigo para que se supere

--- spec/ppt_spec.rb
+++ spec/ppt_spec.rb
@@ -2,13 +2,21 @@ require 'ppt'
 
 describe PiedraPapelTijeras do
 
+    let (:humano) { :piedra }
+    let (:maquina) { :tijeras }
+
     before :all do
-        @ppt_obj = PiedraPapelTijeras.new(:piedra)
+        @ppt_obj = PiedraPapelTijeras.new(humano, maquina)
     end
     
     it "Debe existir una tirada para el humano" do
-        @ppt_obj.humano.should == :piedra
+        @ppt_obj.humano.should == humano
     end
+    
+    if "Debe existir una tirada para la maquina" do
+        @ppt_obj.maquina.should == maquina
+    end
+    
         
 
 end
