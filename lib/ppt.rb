7c94906f88717992148306caa1cf6579f4d7dc35
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-10-26 16:53:26 +0100

    Implementado el septimo test y el codigo de clase necesario para superarlo

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -1,19 +1,20 @@
 class PiedraPapelTijeras
-    attr_reader :humano, :maquina, :tiradas, :ganadoras, :resultados
-    def initialize (human, machine, plays, wins, results)
+    attr_reader :humano, :maquina, :tiradas, :ganadoras, :resultados, :resultado
+    def initialize (human, machine, plays, wins, results, result)
         @humano = human
         @maquina = machine
         @tiradas = plays
         @ganadoras = wins
         @resultados = results
+        @resultado = result
     end
-    
+
     def obtener_humano
         return @humano
     end
-    
+
     def obtener_maquina
         @tiradas.sample
     end
-    
+
 end
--- spec/ppt_spec.rb
+++ spec/ppt_spec.rb
@@ -7,36 +7,41 @@ describe PiedraPapelTijeras do
     let (:tiradas) { [:piedra, :papel, :tijeras] }
     let (:ganadoras) { {:piedra => :tijeras, :papel => :piedra, :tijeras => :papel} }
     let (:resultados) { [:gane, :perdi, :empate] }
+    let (:resultado) { :gane }
 
     before :all do
-        @ppt_obj = PiedraPapelTijeras.new(humano, maquina, tiradas, ganadoras, resultados)
+        @ppt_obj = PiedraPapelTijeras.new(humano, maquina, tiradas, ganadoras, resultados, resultado)
     end
-    
+
     it "Debe existir una tirada para el humano" do
         @ppt_obj.humano.should == humano
     end
-    
+
     it "Debe existir una tirada para la maquina" do
         @ppt_obj.maquina.should == maquina
     end
-    
+
     it "Debe existir una lista de jugadas posibles y quien gana" do
         @ppt_obj.tiradas.should == tiradas
         @ppt_obj.ganadoras.should == ganadoras
     end
-    
+
     it "Se debe invocar al metodo obtener_humano() para recoger la tirada del humano y que esta sea valida" do
         @ppt_obj.tiradas.include? @ppt_obj.obtener_humano
     end
-    
+
     it "Se debe invocar al metodo obtener_maquina() para recoger la tirada de la maquina y que esta sea valida" do
         @ppt_obj.tiradas.include? @ppt_obj.obtener_maquina
     end
-    
+
     it "Debe existir una lista de resultados de un juego desde el punto de vista de la maquina" do
         @ppt_obj.resultados.should == resultados
     end
-    
-        
+
+    it "Debe existir un resultado para un juego, desde el punto de vista de la maquina" do
+        @ppt_obj.resultado.should == resultado
+    end
+
+
 
 end
